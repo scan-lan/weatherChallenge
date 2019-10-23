c0cc0dcb6913057e5162d82712895b6b928fcf19
scan-lan <luke.scanlan@postcodelottery.co.uk>
2019-10-23 13:51:14 +0100

    added array summing function and replaced variables storing min/max temps and returned an object implicitly

--- getWeather.js
+++ getWeather.js
@@ -3,41 +3,35 @@ const axios = require('axios');
 
 const {city} = argv;
 
+const sumTemperatureArray = (fiveDaysWeatherData, minOrMax) => fiveDaysWeatherData.reduce((tot, weatherDay) => tot + weatherDay[`${minOrMax}_temp`], 0);
+
 const displayError = error => {
     console.error(`Sorry, there was a problem fetching the weather for ${city}`);
     console.error(error);
 };
 
 const displayMinMaxTemperatures = ({min, max}) => {
-    console.log(`The average five day forecast temperatures for ${city} are
-  Minimum: ${min} degrees °C
-  Maximum: ${max} degrees °C`);
+    console.log(`The average five day forecast temperatures for ${city} are:
+  Minimum: ${min}°C
+  Maximum: ${max}°C`);
 };
 
-async function fetchLocationDetails(city) {
-    try {
-        const locationDetails = await axios.get(`https://www.metaweather.com/api/location/search/?query=${city}`);
-        return locationDetails.data[0];
-    } catch (error) {
-        displayError(error);
-    }
-}
+const fetchLocationDetails = async city => {
+    const locationDetails = await axios.get(`https://www.metaweather.com/api/location/search/?query=${city}`);
+    return locationDetails.data[0];
+};
 
 const extractLocationId = locationDetails => locationDetails.woeid;
 
 const fetchFiveDayWeather = async locationId => {
-    let fiveDayWeather = await axios.get(`https://www.metaweather.com/api/location/${locationId}`)
+    let fiveDayWeather = await axios.get(`https://www.metaweather.com/api/location/${locationId}`);
     return fiveDayWeather.data.consolidated_weather;
-}
+};
 
-const extractFiveDayAverageMinMaxTemperatures = fiveDaysWeatherData => {
-    minTempTotal = fiveDaysWeatherData.reduce((tot, weatherDay) => tot + weatherDay.min_temp, 0)
-    maxTempTotal = fiveDaysWeatherData.reduce((tot, weatherDay) => tot + weatherDay.max_temp, 0)
-    return {
-        min: (minTempTotal / fiveDaysWeatherData.length).toFixed(2),
-        max: (maxTempTotal / fiveDaysWeatherData.length).toFixed(2)
-    }
-}
+const extractFiveDayAverageMinMaxTemperatures = fiveDaysWeatherData => ({
+    min: (sumTemperatureArray(fiveDaysWeatherData, "min") / fiveDaysWeatherData.length).toFixed(2),
+    max: (sumTemperatureArray(fiveDaysWeatherData, "max") / fiveDaysWeatherData.length).toFixed(2)
+});
 
 fetchLocationDetails(city)
     .then(extractLocationId)
